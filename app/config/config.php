212e9a534687129994b9c8f523d9a95d47a68147
alireza <hamzehzadehalireza@gmail.com>
2023-03-31 16:59:51 +0000

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocb3jzdppmyx',
+  'passwordsalt' => 'MX1lPs+WO+yJs1pRTzneIUTnM7dgxp',
+  'secret' => 'M2vxckNT8tnx6oNo4pOmpjW2GGeFYg1Q6s8XLRtUNwqEyUIS',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.56.104:81',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '26.0.0.11',
+  'overwrite.cli.url' => 'http://192.168.56.104:81',
+  'installed' => true,
+);
